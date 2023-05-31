da22e1cfdc506b80e357c02d3ee706fd7e560b45
osahraoui70 <osahraoui70@gmail.com>
2023-05-31 16:09:05 +0100

    Ajoutez des fichiers projet.

---
+++ Project/Areas/Identity/Data/AppplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Project.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the AppplicationUser class
+public class AppplicationUser : IdentityUser
+{
+    [PersonalData]
+    [Column(TypeName ="nvarchar(100)")]
+    public String Fullname { get; set; }
+
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    [EnumDataType(typeof(MyEnum))]
+    public String Role { get; set; }
+}
+public enum MyEnum
+{
+    Admin,
+    Manager,
+    Member
+}
---
+++ Project/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Project.Areas.Identity.Data;
+using Project.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AuhtDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AuhtDbContextConnection' not found.");
+
+builder.Services.AddDbContext<AuhtDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<AppplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<AuhtDbContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();;
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+app.Run();
